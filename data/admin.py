df0ba78755876b34852786aaac33eab5d0826105
Kollen Gruizenga <64032109+Awes35@users.noreply.github.com>
2022-09-29 13:19:19 -0400

    Initial Database config/setup, Added app, New template & view/url pages (#3)
    * Added pytest & dependencies
    
    * Created data app, Setup DBs, Routing schemes, new view/url pages

---
+++ data/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import HighImpactExperiences, Departments, Faculty, Courses, Majors, Minors
+
+# Register your models here.
+#admin.site.register(HighImpactExperiences)
