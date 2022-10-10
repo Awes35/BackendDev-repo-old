5b2fa94ad1edbcbaa947516df719d16a52fa6bb5
Aaron Ripley <ripleya1@xavier.edu>
2022-10-10 09:22:20 -0400

    Add all other models to viewsets

--- BackendDev/urls.py
+++ BackendDev/urls.py
@@ -20,12 +20,18 @@ from data import views as data_views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'HIEs', data_views.HIEViewSet)
+router.register(r'HighImpactExperiences', data_views.HighImpactExperiencesViewSet) #access: localhost:8000/api/HighImpactExperiences
+router.register(r'Departments', data_views.DepartmentsViewSet) #access: localhost:8000/api/Departments
+router.register(r'Faculty', data_views.FacultyViewSet) #access: localhost:8000/api/Faculty
+router.register(r'Courses', data_views.CoursesViewSet) #access: localhost:8000/api/Courses
+router.register(r'Majors', data_views.MajorsViewSet) #access: localhost:8000/api/Majors
+router.register(r'Minors', data_views.MinorsViewSet) #access: localhost:8000/api/Minors
+router.register(r'Students', data_views.StudentsViewSet) #access: localhost:8000/api/Students
 
 urlpatterns = [
     path('admin/', admin.site.urls), #access: localhost:8000/admin/
     path('', main_views.index, name='index'), #access: localhost:8000/
     path('files/', main_views.files, name='files'), #access: localhost:8000/files/
     #path('api/', data_views.api, name='api'), #access: localhost:8000/api/
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)) #access: localhost:8000/api/[router.url]
 ]
--- data/views.py
+++ data/views.py
@@ -1,13 +1,43 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from data.serializers import SerializerHighImpactExperiences
-from data.models import HighImpactExperiences
+from data.serializers import SerializerCourses, SerializerDepartments, SerializerFaculty, SerializerHighImpactExperiences, SerializerMajors, SerializerMinors, SerializerStudents
+from data.models import Courses, Departments, Faculty, HighImpactExperiences, Majors, Minors, Students
 from rest_framework import viewsets, permissions
 
 def api(request):
     return HttpResponse("API Home Page")
 
-class HIEViewSet(viewsets.ModelViewSet):
+class HighImpactExperiencesViewSet(viewsets.ModelViewSet):
     queryset = HighImpactExperiences.objects.all()
     serializer_class = SerializerHighImpactExperiences
+    permission_classes = [permissions.AllowAny]
+
+class DepartmentsViewSet(viewsets.ModelViewSet):
+    queryset = Departments.objects.all()
+    serializer_class = SerializerDepartments
+    permission_classes = [permissions.AllowAny]
+
+class FacultyViewSet(viewsets.ModelViewSet):
+    queryset = Faculty.objects.all()
+    serializer_class = SerializerFaculty
+    permission_classes = [permissions.AllowAny]
+
+class CoursesViewSet(viewsets.ModelViewSet):
+    queryset = Courses.objects.all()
+    serializer_class = SerializerCourses
+    permission_classes = [permissions.AllowAny]
+
+class MajorsViewSet(viewsets.ModelViewSet):
+    queryset = Majors.objects.all()
+    serializer_class = SerializerMajors
+    permission_classes = [permissions.AllowAny]
+
+class MinorsViewSet(viewsets.ModelViewSet):
+    queryset = Minors.objects.all()
+    serializer_class = SerializerMinors
+    permission_classes = [permissions.AllowAny]
+
+class StudentsViewSet(viewsets.ModelViewSet):
+    queryset = Students.objects.all()
+    serializer_class = SerializerStudents
     permission_classes = [permissions.AllowAny]
