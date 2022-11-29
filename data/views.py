4029752c5280b9f653c24f3a5f40fa8a9b6e4687
Kollen Gruizenga <gruizengak@xavier.edu>
2022-11-29 02:07:38 -0500

    Heavily revised viewsets pertaining to user-related models, adjusted permissions access & querysets for all viewsets

--- data/views.py
+++ data/views.py
@@ -13,110 +13,64 @@ from rest_framework.response import Response
 
 from django.http import Http404
 from django.core.exceptions import PermissionDenied
-import json
-
 from data.permissions import IsOwner
 
 
 def api(request):
     return HttpResponse("API Home Page")
 
-# class StudentViewSet(viewsets.ModelViewSet):
-#     queryset = Student.objects.all()
-#     serializer_class = StudentSerializer
-#     permission_classes = [permissions.DjangoModelPermissions]
-    
-#     def create(self, request):#MAY NOT NEED
-#         body = json.loads(request.body.decode('utf-8'))
-#         # access_token = body.get("access_token")
-#         # content = body['content']
-#         serializer = StudentSerializer(data=request.data)
-    
-#     def list(self, request):
-#         queryset = Student.objects.all()
-#         serializer = StudentSerializer(queryset, many=True)
-#         return Response(serializer.data)
-
-#     def retrieve(self, request, pk=None):
-#         queryset = Student.objects.all()
-#         user = get_object_or_404(queryset, pk=pk)
-#         self.check_object_permissions(request, user)
-#         serializer = StudentSerializer(user)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-#     def update(self, request, pk=None):#MAY NEED?
-#         queryset = Student.objects.all()
-#         user = get_object_or_404(queryset, pk=pk)
-#         self.check_object_permissions(request, user)
-#         pass
-
-#     def partial_update(self, request, pk=None):
-#         queryset = Student.objects.all()
-#         user = get_object_or_404(queryset, pk=pk)
-#         serializer = StudentSerializer(queryset, partial=True)
-#         return Response(serializer.data)
-
-#     def destroy(self, request, pk=None):
-#         try:
-#             instance = self.get_object()
-#             self.perform_destroy(instance)
-#         except Http404:
-#             pass
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    #anyone can create
+    #Students can RUD + list only themselves
+    #Professors can list all
+    #AdminAssistants can update, destroy, retrieve, list all
     
     def get_queryset(self):
-        #filter queryset to only the authenticated user
+        #filter queryset to restrict access scope for the requesting user
         try:
-            if self.action in ['retrieve', 'update', 'partial_update', 'destroy', 'list']:
-                user = self.request.user
-                if user.is_anonymous:
-                    raise PermissionDenied("Request must have an authenticated user.")
-                
-                if user.groups.filter(name__in=["Professor", "AdminAssistant"]).exists():
-                    return Student.objects.all()
-                else:
+            user = self.request.user
+            if user.is_anonymous:
+                raise PermissionDenied("Request must have an authenticated user.")
+            
+            if self.action in ['list', 'retrieve']:
+                if user.groups.filter(name__in=["Student"]).exists():
                     prof = Profile.objects.get(user=user)
                     return Student.objects.filter(prof=prof)
-            else: #for create action
-                return Student.objects.all()
+            
+            #else, for other actions:
+            return Student.objects.all()
+        
         except Http404:
-            # raise Http404("Fail")
             pass
-    
-    #https://stackoverflow.com/questions/39392007/django-rest-framework-viewset-permissions-create-without-list
-    #https://r-future.github.io/post/custom-permission-for-different-action-in-an-viewset/
-    #https://www.django-rest-framework.org/api-guide/serializers/#writable-nested-representations
 
     def get_permissions(self):
         if self.action == 'create': #let anyone (unauthenticated) do 'create' action
             self.permission_classes = [permissions.AllowAny]
-        elif self.action in ['retrieve', 'update', 'partial_update', 'destroy', 'list']:
+        elif self.action in ['list']:
+            self.permission_classes = [permissions.DjangoModelPermissions]
+        elif self.action in ['retrieve', 'update', 'partial_update', 'destroy']:
             self.permission_classes = [IsOwner | permissions.DjangoModelPermissions] 
         
         return [permission() for permission in self.permission_classes] 
-
-    #https://testdriven.io/blog/django-permissions/ -- maybe done in serializer?
-    #https://www.webforefront.com/django/permissionchecks.html
-    #https://stackoverflow.com/questions/29780060/trying-to-parse-request-body-from-post-in-django
     
     def destroy(self, request, pk=None):
-        try:
-            if pk is not None:
+        if pk is not None:
+            try:
                 s = Student.objects.get(id=pk)
+                self.check_object_permissions(self.request, s)
                 p = Profile.objects.get(pk=s.prof_id)
                 u = User.objects.get(pk=p.user_id)
                 u.delete() #-- this cascade deletes Student/Profile too
-            else:
-                u = self.request.user
-                u.delete()
-        except Http404:
-            pass
+            except Student.DoesNotExist:
+                raise Http404("No Student matches given PK")
+        else:
+            s = self.get_object() #--checks obj perms too
+            u = self.request.user #check perms?
+            u.delete()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -125,13 +79,15 @@ class StudentViewSet(viewsets.ModelViewSet):
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.DjangoModelPermissions]
+
 
 
 class MajorViewSet(viewsets.ModelViewSet):
     queryset = Major.objects.all()
     serializer_class = MajorSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.DjangoModelPermissions]
+
 
 
 class MinorViewSet(viewsets.ModelViewSet):
@@ -140,16 +96,108 @@ class MinorViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.DjangoModelPermissions]
 
 
+
 class ProfessorViewSet(viewsets.ModelViewSet):
     queryset = Professor.objects.all()
     serializer_class = ProfessorSerializer
-    permission_classes = [permissions.AllowAny]
+    #Students can retrieve/list all
+    #Professors can UD only themselves, retrieve/list all 
+    #AdminAssistants can CRUD + list all
+
+    def get_queryset(self):
+        #filter queryset to restrict access scope for the requesting user
+        try: 
+            user = self.request.user
+            if user.is_anonymous:
+                raise PermissionDenied("Request must have an authenticated user.")
+            
+            if self.action in ['list', 'retrieve']:
+                # if user.groups.filter(name__in=["Student"]).exists():
+                #     raise PermissionDenied("Must be authenticated user w perms.")
+                # else: #AdminAssistants
+                    # return Professor.objects.all()
+                pass
+            
+            #else, for other actions:
+            return Professor.objects.all()
+
+        except Http404:
+            pass
+
+    def get_permissions(self):
+        if self.action in ['create', 'list']:
+            self.permission_classes = [permissions.DjangoModelPermissions]
+        elif self.action in ['retrieve', 'update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsOwner | permissions.DjangoModelPermissions] 
+        return [permission() for permission in self.permission_classes] 
+    
+    def destroy(self, request, pk=None):
+        if pk is not None:
+            try:
+                pf = Professor.objects.get(id=pk)
+                self.check_object_permissions(self.request, pf)
+                p = Profile.objects.get(pk=pf.prof_id)
+                u = User.objects.get(pk=p.user_id)
+                u.delete() #-- this cascade deletes Professor/Profile too
+            except Professor.DoesNotExist:
+                raise Http404("No Professor matches given PK")
+        else:
+            pf = self.get_object() #--checks obj perms too
+            u = self.request.user #check perms?
+            u.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
 class AdminAssistantViewSet(viewsets.ModelViewSet):
     queryset = AdminAssistant.objects.all()
     serializer_class = AdminAssistantSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+    #Professors can retrieve/list all 
+    #AdminAssistants can UD only themselves, can create/retrieve/list all 
+
+    def get_queryset(self):
+        #filter queryset to restrict access scope for the requesting user
+        try: 
+            user = self.request.user
+            if user.is_anonymous:
+                raise PermissionDenied("Request must have an authenticated user.")
+            
+            if self.action in ['list', 'retrieve']:
+                if user.groups.filter(name__in=["Student"]).exists(): #dont allow Students to list/retrieve
+                    raise PermissionDenied("Must be authenticated user w perms.")
+                return AdminAssistant.objects.all()
+            
+            #else, for other actions:
+            return AdminAssistant.objects.all()
+
+        except Http404:
+            pass
+
+    def get_permissions(self):
+        if self.action in ['create', 'list']:
+            self.permission_classes = [permissions.DjangoModelPermissions]
+        elif self.action in ['retrieve', 'update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsOwner | permissions.DjangoModelPermissions] 
+        return [permission() for permission in self.permission_classes] 
+    
+    def destroy(self, request, pk=None):
+        if pk is not None:
+            try:
+                a = AdminAssistant.objects.get(id=pk)
+                self.check_object_permissions(self.request, a)
+                p = Profile.objects.get(pk=a.prof_id)
+                u = User.objects.get(pk=p.user_id)
+                u.delete() #-- this cascade deletes AdminAssistant/Profile too
+            except AdminAssistant.DoesNotExist:
+                raise Http404("No AdminAssistant matches given PK")
+        else:
+            a = self.get_object() #--checks obj perms too
+            u = self.request.user #check perms?
+            u.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -158,13 +206,16 @@ class CourseViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.DjangoModelPermissions]
 
 
+
 class HighImpactExperienceViewSet(viewsets.ModelViewSet):
     queryset = HighImpactExperience.objects.all()
     serializer_class = HighImpactExperienceSerializer
     permission_classes = [permissions.DjangoModelPermissions]
 
 
+
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [permissions.DjangoModelPermissions] 
+
