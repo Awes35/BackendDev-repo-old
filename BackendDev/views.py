a3aed1dbb51571e48cc7fec36b564232c372ffb2
Kollen Gruizenga <gruizengak@xavier.edu>
2022-11-29 23:48:32 -0500

    Add view functions for displaying URL path options

--- BackendDev/views.py
+++ BackendDev/views.py
@@ -2,12 +2,51 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from data.models import HighImpactExperience #temp
 from django.core import serializers #temp
-from data.serializers import HighImpactExperienceSerializer
+from data.serializers import HighImpactExperienceSerializer #temp
 import json
 from django.contrib.auth import authenticate, login
 
-def index(request):
-    return HttpResponse("Default Landing Page")
+from django.urls import URLPattern, URLResolver
+from django.conf import settings
+from BackendDev.settings import RESTRICTED_PATHS
+
+def list_urls(lis, acc=None):
+    if acc is None:
+        acc = []
+    if not lis:
+        return
+    l = lis[0]
+    if isinstance(l, URLPattern):
+        yield acc + [str(l.pattern)]
+    elif isinstance(l, URLResolver):
+        yield from list_urls(l.url_patterns, acc + [str(l.pattern)])
+    yield from list_urls(lis[1:], acc)
+
+
+def urlpaths(request):
+    urlconf = __import__(settings.ROOT_URLCONF, {}, {}, [''])
+    path_lst = ["-- Server Endpoint Paths --"]
+    for p in list_urls(urlconf.urlpatterns):
+        # print(f"\nBefore: {p}")
+        skip=False
+        for rp in RESTRICTED_PATHS:
+            if rp in str(p):
+                skip=True
+        if skip:
+            # print(f"SKIPPING <{p}>")
+            continue
+
+        p = "".join(p)
+        
+        p = str(p).replace("(?P<pk>[^/.]+)", "<int:pk>")
+        p = str(p).replace("^", "")
+        p = str(p).replace("$", "")
+        # print(f"After: {p}")
+
+        path_lst = path_lst + [p]
+    
+    return HttpResponse("\n".join(path_lst), content_type="text/plain")
+
 
 def files(request):
     return render(request, 'BackendDev/files.html',
